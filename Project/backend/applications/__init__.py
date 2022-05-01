06e5be44124d01457c68304452bd44e378ae478e
rahulshresthaa <32300204+rahulshresthaa@users.noreply.github.com>
2022-05-01 19:15:16 +0100

    Classification Models , SMOTE + Backend Routes

---
+++ Project/backend/applications/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+from application.models import *
+from application.routes import *
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/zipped'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+ma = Marshmallow(app)
+CORS(app)
+
+from application.models import *
+
+from application.routes import *
